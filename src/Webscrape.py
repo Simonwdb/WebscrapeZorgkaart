c33ce4c5cdcbf54c200bc24cf9ed9eff0c9d61bc
simonwdb <simonwdeboer@gmail.com>
2025-05-22 16:27:35 +0200

    add Webscrape class with basic HTTP request setup and facility URL handler

---
+++ src/Webscrape.py
@@ -0,0 +1,26 @@
+import time
+import requests
+import bs4
+
+from typing import Dict, List, Union
+
+
+class Webscrape:
+    def __init__(self, base_url: str) -> None:
+        self.base_url: str = base_url
+        self.headers: dict[str, str] = {
+            'User-Agent': (
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                'AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/114.0.0.0 Safari/537.36'
+            )
+        }
+    
+    def get_filter_result(self, facility: str) -> Union[List[bs4.element.Tag], None]:
+        temp_url: str = f'{self.base_url}/{facility.lower()}'
+        response = requests.get(url=temp_url, headers=self.headers)
+
+        if response.status_code != 200:
+            return None
+        
+        
