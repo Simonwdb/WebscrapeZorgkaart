92d3dd0352373f53a622c6b467bc89cc33108138
simonwdb <simonwdeboer@gmail.com>
2025-05-27 10:55:37 +0200

    changed the settings in settings.py

--- ZorgkaartScrapy/ZorgkaartScrapy/settings.py
+++ ZorgkaartScrapy/ZorgkaartScrapy/settings.py
@@ -1,93 +1,43 @@
 # Scrapy settings for ZorgkaartScrapy project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = "ZorgkaartScrapy"
 
 SPIDER_MODULES = ["ZorgkaartScrapy.spiders"]
 NEWSPIDER_MODULE = "ZorgkaartScrapy.spiders"
 
-ADDONS = {}
+# Identificeer jezelf netjes
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/114.0.0.0 Safari/537.36"
+)
 
+# Niet verplicht robots.txt te volgen, anders blokkeert zorgkaart alles
+ROBOTSTXT_OBEY = False
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "ZorgkaartScrapy (+http://www.yourdomain.com)"
+# Downloadvertraging tussen requests
+DOWNLOAD_DELAY = 1  # seconden
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# Maximaal aantal gelijktijdige requests per domein
+CONCURRENT_REQUESTS_PER_DOMAIN = 4
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# AutoThrottle inschakelen voor dynamische snelheid
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 10
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2
+AUTOTHROTTLE_DEBUG = False
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# Cookies uitgeschakeld (indien niet nodig)
+COOKIES_ENABLED = False
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# Pipelines activeren om naar Excel te schrijven
+ITEM_PIPELINES = {
+    "ZorgkaartScrapy.pipelines.ExcelExportPipeline": 300,
+}
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "ZorgkaartScrapy.middlewares.ZorgkaartscrapySpiderMiddleware": 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "ZorgkaartScrapy.middlewares.ZorgkaartscrapyDownloaderMiddleware": 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "ZorgkaartScrapy.pipelines.ZorgkaartscrapyPipeline": 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-
-# Set settings whose default value is deprecated to a future-proof value
+# Excel-pipeline vereist UTF-8 encoding
 FEED_EXPORT_ENCODING = "utf-8"
+
+# Logging (optioneel)
+LOG_LEVEL = "INFO"
