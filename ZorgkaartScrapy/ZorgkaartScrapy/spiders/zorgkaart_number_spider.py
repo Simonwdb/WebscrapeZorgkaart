30fd20bed9b1779b91c13f2f66dba6b9dd3dd277
simonwdb <simonwdeboer@gmail.com>
2025-07-07 10:15:31 +0200

    implement parsing logic to extract specialist count from zorgkaart pages

--- ZorgkaartScrapy/ZorgkaartScrapy/spiders/zorgkaart_number_spider.py
+++ ZorgkaartScrapy/ZorgkaartScrapy/spiders/zorgkaart_number_spider.py
@@ -1,6 +1,7 @@
 import json
 import scrapy
 import datetime
+import numpy as np
 
 from pathlib import Path
 from typing import Generator, Dict, Any
@@ -10,7 +11,7 @@ class ZorgkaartNumberSpiderSpider(scrapy.Spider):
     name = "zorgkaart_number_spider"
     allowed_domains = ["zorgkaartnederland.nl"]
 
-    def start_requests(self):
+    def start_requests(self) -> Generator[scrapy.Request, None, None]:
         json_path = Path("data/zorgkaart_details_update.json")
 
         if not json_path.exists():
@@ -35,5 +36,24 @@ class ZorgkaartNumberSpiderSpider(scrapy.Spider):
                 )
             break # for now, just only scrape the first item from target_organisaties
 
-    def parse(self, response):
-        pass
+    def parse(self, response: scrapy.http.Response) -> Generator[Dict[str, Any], None, None]:
+        base_url = response.meta['base_url']
+
+        try:
+            a_tags = response.css("div.filter__asside__item a")
+            if len(a_tags) > 1:
+                a_tag = a_tags[1]
+                count_text = a_tag.css("span.filter-radio__counter::text").get()
+                count = int(count_text.strip("() \n")) if count_text else None
+            else:
+                count = None
+            
+            print(f"✅ {base_url}/specialisten → {count if count is not None else "geen"} specialisten gevonden")
+            yield {
+                "base_url": base_url,
+                "specialisten_count": count
+            }
+
+        except Exception as e:
+            print(f"⚠️ Fout bij parsen {response.url}: {e}")
+
